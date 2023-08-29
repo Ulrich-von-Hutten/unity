7ec209dc30495f006d48f65539cd6e2cc1f5602e
Ulrich-von-Hutten <3576696848@qq.com>
2023-08-29 15:49:06 +0800

    80 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -12,6 +12,7 @@ public class LoadAssets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
         Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
         Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
