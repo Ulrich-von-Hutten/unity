5b0e9c1c475d31ee49e216959180eaeb191ef6c6
Ulrich-von-Hutten <3576696848@qq.com>
2023-08-29 16:02:42 +0800

    complete 90 percent

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     int i = 3;
     public Renderer rend;
